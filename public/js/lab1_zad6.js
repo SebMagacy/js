f3837e8095bd5eb986b7912de06e6d6813724f72
SebMagacy <rogackis@gmail.com>
2026-06-04 12:46:20 +0200

    Dodano plik lab1_zad6.js oraz zaktualizowano trasę do nowego zadania w pliku web.php

---
+++ public/js/lab1_zad6.js
@@ -0,0 +1,11 @@
+console.log("=== Zadanie 6 ===");
+
+const dodaj = (a, b) => a + b;
+
+const kwadrat = x => x * x;
+
+console.log("Suma 2 + 3 =", dodaj(2, 3));
+console.log("Suma 10 + 5 =", dodaj(10, 5));
+
+console.log("Kwadrat liczby 4 =", kwadrat(4));
+console.log("Kwadrat liczby 7 =", kwadrat(7));
