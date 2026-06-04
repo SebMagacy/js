0b01c07d3a7587ca09a00d819b9d847693f6d3b5
SebMagacy <rogackis@gmail.com>
2026-06-04 12:56:29 +0200

    Dodano plik lab1_zad7.js oraz zaktualizowano trasę do nowego zadania w pliku web.php

---
+++ public/js/lab1_zad7.js
@@ -0,0 +1,12 @@
+console.log("=== Zadanie 7 ===");
+
+const zadania = ["Nauka HTML", "Nauka CSS"];
+
+console.log("Tablica przed dodaniem elementów:");
+console.log([...zadania]);
+
+zadania.push("Nauka JavaScript");
+zadania.push("Wykonanie laboratorium");
+
+console.log("Tablica po dodaniu elementów:");
+console.log([...zadania]);
--- resources/views/lab1.blade.php
+++ resources/views/lab1.blade.php
@@ -13,7 +13,7 @@
         zakładki <strong>Console</strong>.</p>
     <p>Kod do zadań wpisuj w znaczniku <code>&lt;script&gt;</code> na dole
         pliku.</p>
-    <script src="{{ asset('js/lab1_zad6.js') }}"></script>
+    <script src="{{ asset('js/lab1_zad7.js') }}"></script>
 </body>
 
 </html>
